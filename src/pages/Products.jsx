5dd62e4634d7c891de4ccf3e4afebf89b0316c49
unknown <raquel.haagsma@gmail.com>
2024-09-09 18:25:48 -0300

    fix: home page and band products

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,31 +1,40 @@
 import Card from 'src/components/card';
 import Layout from 'src/components/layout';
 import React, { useEffect, useState } from 'react';
-import { values, values2 } from './fake';
+import { values, values2, bands, products } from './fake';
 import { getAllProduct } from 'src/services/http-commons';
 import Search from 'src/components/search';
+import { useParams } from 'react-router-dom';
 
 const Products = () => {
     const [data, setData] = useState([]);
+    const [band, setBand] = useState({})
+    const { bandId } = useParams()
 
     async function initialize() {
-        try {
-            const res = await getAllProduct()
-            setData(res)
-        } catch (error) {
-            setData(values,values2);   
+        if(bandId) {
+            try {
+                const res = await getAllProduct()
+                // setData(res.filter(el => el.bands.includes(bandId))) <- descomentar para usar a API
+                setData(products.filter(el => el.bands.includes(bandId)))
+            } catch (error) {
+                setData(products.filter(el => el.bands.includes(bandId)))
+                // setData(values,values2);   
+            }
+            const band = bands.find(el => el.id === bandId)
+            setBand(band)
         }
     }
 
     useEffect(() => {
         initialize()
-    }, [])
+    }, [bandId])
 
     return (
         <Layout>
-            <Search pageName="Bandas"/>
+            <Search pageName={band?.name} description={band?.description} image={band?.image}/>
             <div className='grid grid-cols-4 gap-4'>
-                {data.map(el => <Card id={el.id} image={el.image} name={el.name} price={el.price}/>)}
+                {data?.map(el => <Card id={el?.id} image={el?.image} name={el?.name} price={el?.price}/>)}
                 {!data.length ? <p>Nenhum produto encontrado</p> : null}
             </div>
         </Layout>
--- src/pages/fake.js
+++ src/pages/fake.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 export const values = [{ 
     id: '7d3850f2-00ce-4d2a-8ec5-75770e069b4e',
     name: 'Blusa joia',
@@ -19,3 +21,43 @@ export const values2 = [{
         name: 'Blusa'
     } 
 }]
+
+export const bands = [
+    {
+        id: uuidv4(),
+        name: "Banda 1",
+    },
+    {
+        id: uuidv4(),
+        name: "Banda 2",
+    },
+    {
+        id: uuidv4(),
+        name: "30PRAUM",
+        description: "A 30PRAUM é a produtora e selo musical fundada pelo rapper Matuê junto a sua sócia Clara Mendes em Fortaleza-CE. Criada em 2016, a '30' - como é intimamente referenciada pelos milhões de fãs espalhados pelo Brasil - tem sido pioneira na fomentação da música e cultura urbana no Nordeste, além de ter se tornado, nos últimos anos, uma referência em excelência artística e sucesso comercial no país, posicionando o rap e o trap nordestinos no topo dos charts nacionais. Seu casting, composto atualmente pelos artistas Matuê, WIU e Teto, representa uma das grandes potências do cenário musical brasileiro, acumulando bilhões de plays em hits como 'Vampiro', 'Coração de Gelo', 'Fim de Semana no Rio' e 'Conexões de Máfia', além de uma agenda de shows esgotados dentro e fora do país. Com um direcionamento artístico autêntico e estratégias criativas que buscam inovar e renovar os padrões da indústria, a 30PRAUM já carrega, em poucos anos de existência, um histórico de recordes e resultados impressionantes, que se tornaram cases de referência no mercado musical. Atualmente, a equipe 30PRAUM é composta por mais de 60 colaboradores, distribuídos entre sua sede em Fortaleza e uma filial em São Paulo. Os talentos de perfis diversos e marcados pela pluralidade se unem em duas características principais: a paixão por criar experiências memoráveis para os fãs e a ambição de deixar sua marca na história da música e da arte no Brasil. É A 30 NO COMANDO! — Siga a 30PRAUM e seus artistas nas redes sociais: Instagram: @30praum | @matue | @hotwiu30 | @euteto",
+        image: "https://i0.wp.com/www.rapdab.com.br/wp-content/uploads/2022/01/30PRAUM-MATUE-PODPAH.jpg?fit=1250%2C750&ssl=1"
+    },
+    {
+        id: uuidv4(),
+        name: "Banda 4",
+    },
+    {
+        id: uuidv4(),
+        name: "Banda 5",
+    },
+]
+
+export const products = [
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 1', price: 100, bands: [bands[0].id]}, 
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 2', price: 200, bands: [bands[1].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 3', price: 300, bands: [bands[2].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 4', price: 400, bands: [bands[3].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 5', price: 500, bands: [bands[4].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 6', price: 600, bands: [bands[0].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 1', price: 100, bands: [bands[1].id]}, 
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 2', price: 200, bands: [bands[2].id, bands[0].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 3', price: 300, bands: [bands[3].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 4', price: 400, bands: [bands[4].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 5', price: 500, bands: [bands[0].id]},
+    {id: uuidv4(), image: 'https://via.placeholder.com/150', name: 'Produto 6', price: 600, bands: [bands[1].id]},
+] 
