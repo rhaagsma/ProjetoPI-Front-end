ea6691971121b1d613c35e7feac0e384b38a790c
Numseinemmeunome <alannakamura123@gmail.com>
2024-09-13 11:01:37 -0300

    adicionado componente carrossel

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -2,14 +2,13 @@ import React from "react"
 import './Hero.css'
 import gati from "../assets/gati firte.png"
 import { Navigate, Link } from "react-router-dom"
+import  MultipleItems  from "../MultipleItems"
+import Layout from "../layout"
 const Hero = () => {
     return (
-        // <div className="flex items-center justify-center h-[50vh] py-7">
-        //     <div className="w-full flex max-w-7xl h-full rounded-md bg-slate-300 items-center justify-center">
-        //         <h1 className="text-slate-700">Carrosel</h1>
-        //     </div>
-        // </div>
-        <div/>
+        <Layout>
+            <MultipleItems/>
+        </Layout>
     )
 }
 
